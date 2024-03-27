2b1d5a8ef44104b18b7d7e241c986f5aed0bbf0b
DAVID EZEQUIEL WEISSBROD <47436755@ort>
2024-03-27 10:38:58 -0300

    faltan dos funciones del js y el diseño - html listo

---
+++ script.js
@@ -0,0 +1,45 @@
+var tareas = []
+
+function borrarTarea(){
+    tareas = document.getElementById("lista")
+    for(var i = 0; i < tareas.length; i++){
+        var span = document.createElement("SPAN")
+        var txt = document.createElement("\u00D7")
+        span.className = "cerrar"
+        span.appendChild(txt)
+        tareas[i].appendChild(span)
+    }
+}
+
+function esconderTarea(){
+    var cerrar = document.getElementById("cerrar")
+    for(var i = 0; i < cerrar.length; i++){
+        cerrar[i].onclick() = function(){
+            var div = this.parentElement;
+        }
+    }
+}
+
+function checkTarea(){
+    var lista = document.querySelector("ul", check(ev))
+    lista.addEventListener('click')
+}
+
+function check(ev){
+    if(ev.target.tagName === 'li'){
+        ev.target.classList.toggle('checked')
+    }
+}
+
+function añadirTarea(){
+    var li = document.createElement('li')
+    var value = document.getElementById('tarea').value
+    var txt = document.createTextNode('inputValue')
+    li.appendChild(txt)
+    if (inputValue === '') {
+        alert("Tenes que escribir algo");
+      } else {
+        document.getElementById("list").appendChild(li);
+      }
+      document.getElementById("tarea").value = "";
+}
