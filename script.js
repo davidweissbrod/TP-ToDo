606798bea172c625ee005b6d536846fb427b2455
davidweissbrod <47436755@est.ort.edu.ar>
2024-04-06 20:09:49 -0300

    fix error que no añadia

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
-var tareas = []
-
-function borrarTarea(){
-    tareas = document.getElementById("lista")
-    for(var i = 0; i < tareas.length; i++){
-        var span = document.createElement("SPAN")
-        var txt = document.createTextNode("\u00D7")
-        span.className = "cerrar"
-        span.appendChild(txt)
-        tareas[i].appendChild(span)
-    }
-}
-
-function esconderTarea(){
-    var cerrar = document.getElementById("cerrar")
-    for(var i = 0; i < cerrar.length; i++){
-        cerrar[i].onclick() = function(){
-            var div = this.parentElement;
-        }
-    }
-}
-
-function checkTarea(){
-    var lista = document.querySelector("ul", check(ev))
-    lista.addEventListener('click')
-}
-
-function check(ev){
-    if(ev.target.tagName === 'li'){
-        ev.target.classList.toggle('checked')
-    }
-}
+let tareas = [];
+let tiempoTareas = [];
 
 function añadirTarea(){
-    var li = document.createElement('li')
-    var value = document.getElementById('tarea').value
-    var txt = document.createTextNode('inputValue')
-    li.appendChild(txt)
-    if (inputValue === '') {
-        alert("Tenes que escribir algo")
-    } else {
-        document.getElementById("lista").appendChild(li)
+    var ingresoTarea = document.getElementById("ingresoTarea");
+    var tarea = tareaInput.value.trim();
+    if (tarea != ""){
+      tareas = [];
+      var tiempo = new Date().getTime();
+      tiempoTareas.push({
+          tarea: tarea,
+          tiempoCreacion: tiempo,
+          completada: false,
+          tiempoFinalizacion: null
+        });
+      tareas = tiempoTareas
+      ingresoTarea.value = ""
+      mostrarToDo()
     }
-    document.getElementById("tarea").value = ""
+    else{
+        alert("Tenes que ingresar algo")
+    }
+  }
+
+
+  function mostrarToDo(){
+    var lista = document.getElementById("lista");
+    lista.innerHTML = "";
+    tareas.forEach(tarea => {
+      var li = document.createElement("li");
+      var input = document.createElement("input");
+      input.type = "checkbox";
+      input.checked = tarea.completada;
+      input.onclick = function() {
+        tarea.completada = !tarea.completada;
+        if(tarea.completada){
+          tiempo = new Date().getTime();
+          tarea.tiempoFinalizacion = tiempo;
+        }
+        mostrarToDo();
+      };
+      li.appendChild(input);
+      var txt = document.createTextNode(tarea.tarea);
+  
+      if(tarea.completada){
+        var span = document.createElement("span");
+        span.className = "completed";
+        span.appendChild(txt);
+        li.appendChild(span);
+      }
+      else{
+        li.appendChild(txt);
+      }
+      lista.appendChild(li);
+    });
+  }
+  
+
+function masRapida(){
+    let tareaMasRapida = null;
+    let tiempoMasRapido = Infinity;
+    for(var i = 0; i < tareaMasRapida.length; i++){
+      if(tarea.completada && tiempoMasRapido > (tarea.tiempoFinalizacion - tarea.tiempoCreacion)){
+        tareaMasRapida = tarea;
+        tiempoMasRapido = (tarea.tiempoFinalizacion - tarea.tiempoCreacion)
+      }
+    }
+    alert("La tarea que se termino mas rapido fue " + tareaMasRapida.tarea)
 }
 
-var span = document.createElement("SPAN")
-var txt = document.createTextNode("\u00D7")
-span.className("cerrar")
-span.appendChild(txt)
-li.appendChild(span)
-
-for(var i = 0; i < close.length; i++){
-    close[i].onclick() = function() {
-        var div = this.parentElement
-    }
-}
+function borrar(){
+    tareas = [];
+    tareasTemporal = [];
+    mostrarToDo();
+  }
