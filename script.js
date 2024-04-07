be2c30ebb8b953d479155f57a04faf81bda1fad8
davidweissbrod <47436755@est.ort.edu.ar>
2024-04-07 20:40:22 -0300

    terminado

--- script.js
+++ script.js
@@ -1,75 +1,81 @@
-let tareas = [];
-let tiempoTareas = [];
+var tareas = []
+var tareasTemporal = []
 
 function añadirTarea(){
-    var ingresoTarea = document.getElementById("ingresoTarea");
-    var tarea = tareaInput.value.trim();
-    if (tarea != ""){
-      tareas = [];
-      var tiempo = new Date().getTime();
-      tiempoTareas.push({
-          tarea: tarea,
-          tiempoCreacion: tiempo,
-          completada: false,
-          tiempoFinalizacion: null
-        });
-      tareas = tiempoTareas
-      ingresoTarea.value = ""
-      mostrarToDo()
+  var ingresoTarea = document.getElementById("ingresoTarea")
+  var tarea = ingresoTarea.value.trim()
+  if (tarea != ""){
+    tareas = []
+    console.log(tareas)
+    console.log(ingresoTarea)
+    var tiempo = new Date().getTime()
+    tareasTemporal.push({
+        tarea: tarea,
+        tiempo: tiempo,
+        completada: false,
+        tiempoTerminado: null
+      });
+    tareas = tareasTemporal
+    ingresoTarea.value = ""
+    mostrarTarea()
+  }
+  else{
+    alert("Tenes que escribir algo")
+  }
+}
+
+function mostrarTarea(){
+  var ingresoTarea = document.getElementById("ingresoTarea")
+  ingresoTarea.innerHTML = ""
+  document.getElementById("lista").innerHTML = ""
+  tareas.forEach(tarea => {
+    var li = document.createElement("li")
+    var checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.checked = tarea.completada
+    checkbox.onclick = function() {
+      tarea.completada = !tarea.completada
+      if(tarea.completada){
+        var tiempo = new Date().getTime()
+        tarea.tiempoTerminado = tiempo
+      }
+      mostrarTarea()
+    }
+    li.appendChild(checkbox);
+    var txt = document.createTextNode(tarea.tarea)
+    if(tarea.completada){
+      var span = document.createElement("span")
+      span.className = "completed"
+      span.appendChild(txt)
+      li.appendChild(span)
     }
     else{
-        alert("Tenes que ingresar algo")
+      li.appendChild(txt)
     }
-  }
-
-
-  function mostrarToDo(){
-    var lista = document.getElementById("lista");
-    lista.innerHTML = "";
-    tareas.forEach(tarea => {
-      var li = document.createElement("li");
-      var input = document.createElement("input");
-      input.type = "checkbox";
-      input.checked = tarea.completada;
-      input.onclick = function() {
-        tarea.completada = !tarea.completada;
-        if(tarea.completada){
-          tiempo = new Date().getTime();
-          tarea.tiempoFinalizacion = tiempo;
-        }
-        mostrarToDo();
-      };
-      li.appendChild(input);
-      var txt = document.createTextNode(tarea.tarea);
+    lista.appendChild(li)
+  })
   
-      if(tarea.completada){
-        var span = document.createElement("span");
-        span.className = "completed";
-        span.appendChild(txt);
-        li.appendChild(span);
-      }
-      else{
-        li.appendChild(txt);
-      }
-      lista.appendChild(li);
-    });
-  }
-  
-
-function masRapida(){
-    let tareaMasRapida = null;
-    let tiempoMasRapido = Infinity;
-    for(var i = 0; i < tareaMasRapida.length; i++){
-      if(tarea.completada && tiempoMasRapido > (tarea.tiempoFinalizacion - tarea.tiempoCreacion)){
-        tareaMasRapida = tarea;
-        tiempoMasRapido = (tarea.tiempoFinalizacion - tarea.tiempoCreacion)
-      }
-    }
-    alert("La tarea que se termino mas rapido fue " + tareaMasRapida.tarea)
 }
 
 function borrar(){
-    tareas = [];
-    tareasTemporal = [];
-    mostrarToDo();
+  tareas = []
+  tareasTemporal = []
+  mostrarTarea()
+}
+
+function masRapida() {
+  var tareaMasRapida = null
+  var tiempoMasRapido = Infinity
+  for (var i = 0; i < tareasTemporal.length; i++) {
+    var tarea = tareasTemporal[i]
+    if (tarea.completada && tiempoMasRapido > (tarea.tiempoTerminado - tarea.tiempo)) {
+      tareaMasRapida = tarea
+      tiempoMasRapido = (tarea.tiempoTerminado - tarea.tiempo)
+    }
   }
+  if (tareaMasRapida !== null) {
+    alert("La tarea mas rapida fue " + tareaMasRapida.tarea)
+  } else {
+    alert("No hay tareas completadas")
+  }
+}
